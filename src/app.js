c29136478af1c7e4b612abc411935cc1335b048f
Drzaln <drzaln@null.net>
2020-05-30 17:38:47 +0700

    Club-Finder

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import "regenerator-runtime"
+import "./styles/style.css";
+import "./styles/clublist.css";
+import "./script/data/clubs.js";
+import "./script/component/app-bar.js";
+import main from "./script/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
